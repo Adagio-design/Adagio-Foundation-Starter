4a757f7ccf678abad60d4f7fd1f7ea89ff428697
Adrian Bengtson <adrian@adagio.se>
2015-06-24 15:15:16 +0200

    Let WordPress handle title tag

--- library/theme-support.php
+++ library/theme-support.php
@@ -13,6 +13,14 @@ function adagio_foundation_starter_theme_support() {
 	// Add menu support
 	add_theme_support( 'menus' );
 
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
 	// Add post thumbnail support: http://codex.wordpress.org/Post_Thumbnails
 	add_theme_support( 'post-thumbnails' );
 
