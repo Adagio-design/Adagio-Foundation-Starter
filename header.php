6ad8a2008d4910aa2113b099a23067767e747b93
Adrian Bengtson <adrian@adagio.se>
2015-06-24 15:16:31 +0200

    Use bloginfo charset instead of hard coded utf-8
    (Taken from Underscores.)

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
